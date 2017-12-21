60ca3b8590f46136e79ea4699605f712fde354a4
Chris Lam <christopher.kong.lam@gmail.com>
2017-12-20 16:27:33 -0800

    first commit. commiting completed project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Indecision App from React Web Developer Course [Tutorial](https://www.udemy.com/react-2nd-edition/v4)
+React App that allows users to input actions and have the computer decide which action to take.
+
+Concepts demonstrated:
+* Function Components
+* Stateful Components
+* Saving and fetching from local storage
+* Component Lifecycle methods
+* Passing parent component methods into child components
+* Webpack confguration for component bundling
+* Webpack-dev-server for change watch, live reload, and local serving to browser
+* Babel with env and react presets to transpile es6 to es5
+* Babel transform-class-properties plugin to transform modern class syntax
+* React-Modal third party component using dynamic content
+* Scss for styling using variables
+* SCSS partials
+* Webpack sass-loader and node-sass to transpile and bundle styles
+* Uses rem instead of pixels for styling for better accessability situations
+* Scss Block element modifier (ie header__title): looks for title somewhere inside .header element
+* Normalize.css to reset css for fresh base styling for cross browser styling
+* SCSS darken() function to darken a given color by a certain percentage;
+* BEM (-- for modifier of element && __ for element inside element)
+* Transition in and out modal from opacity 0 to 1 over 200ms
+* Mobile viewport optimization by using meta viewport tag with width matching device width and initial scale 1
+* Media queries: min-width=apply styles for widths greater than specified. Spacing and column layout toggling.
---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import AddOption from './AddOption.js';
+import Options from './Options.js';
+import Action from './Action.js';
+import Header from './Header.js';
+import OptionModal from './OptionModal';
+
+class IndecisionApp extends React.Component {
+    state = {
+        options: [],
+        selectedOption: undefined
+    };
+
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => option !== optionToRemove)
+        }));
+    };
+
+    handleDeleteOptions = () => {
+        this.setState(() => ({
+            options: []
+        }));
+    };
+
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+
+        this.setState(() => ({
+            selectedOption: option
+        }));
+    };
+
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
+        }
+
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    };
+
+    handleClearSelectedOption = () => {
+        this.setState(() => ({
+            selectedOption: undefined
+        }));
+    };
+
+    componentDidMount() {
+        try {
+            console.log('fetching data');
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({ options }));
+            }
+        } catch (e) {
+            //do nothing
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length != this.state.options.length) {
+            console.log('saving data');
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('component did unmount');
+        localStorage.clear();
+    }
+
+    render() {
+        const subtitle = "Put your life in the hands of a computer";
+
+        return (
+            <div>
+                <Header subtitle={subtitle} />
+                <div className="container"> 
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOption={this.handleAddOption}
+                        />
+                    </div>
+                </div>
+
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
+                />
+            </div>
+        )
+    }
+}
+
+IndecisionApp.defaultProps = {
+    options: []
+}
+
+export default IndecisionApp;
