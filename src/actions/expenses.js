aa3fe69f0bd6fd2d1f7f9bdcd9935cc934df1fec
Chris Lam <christopher.kong.lam@gmail.com>
2018-01-09 14:19:12 -0800

    get expenses from firebase on start and related tests

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,9 +33,33 @@ export const removeExpense = ({ id }) => ({
     id
 });
 
+
 //EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+//SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = (expenseData = {}) => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+
+            dispatch(setExpenses(expenses));
+        });
+    }
+}
